4dc9a4ad68f7a4426cbbba0aa590ec1c53f60134
ffontes <fontes.fernando@gmail.com>
2014-05-10 18:23:50 -0300

    plot3

--- plot3.R
+++ plot3.R
@@ -21,8 +21,14 @@ period <- data[data[,1]=="1/2/2007" | data[,1]=="2/2/2007",]
 
 x <- strptime(paste(period$Date, period$Time), "%d/%m/%Y %H:%M:%S")
 
-png("plot2.png")
+png("plot3.png")
 
-plot(x, period$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+plot(x, period$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+
+lines(x, period$Sub_metering_2, col="red")
+lines(x, period$Sub_metering_3, col="blue")
+
+legend('topright',  c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty = c(1,1,1), col = c('black', 'red', 'blue'))
 
 dev.off()
